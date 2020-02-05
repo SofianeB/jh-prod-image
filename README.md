267dd2173f58f20da2a6dfad27adf5ab8be766dd
SofianeB <bendoukha@dkrz.de>
2020-02-05 11:48:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[![](https://images.microbadger.com/badges/license/gridsof/jh-prod-image.svg)](https://microbadger.com/images/gridsof/jh-prod-image "Get your own license badge on microbadger.com")
---
+++ start-notebook.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+set -e
+
+if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
+  # launched by JupyterHub, use single-user entrypoint
+  exec /usr/local/bin/start-singleuser.sh $*
+else
+  if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
+    . /usr/local/bin/start.sh jupyter lab $*
+  else
+    . /usr/local/bin/start.sh jupyter notebook $*
+  fi
+fi
+
+echo export OPH_USER=${JUPYTERHUB_USER}
